8022cd51dcfd65fc55d35d06c7ec6cdb2f47780f
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2022-11-19 14:24:11 -0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "bitpreco",
+      script: "./index.js",
+      watch: ["index.js", ".env"],
+      // Delay between restart
+      watch_delay: 1000,
+      ignore_watch: ["node_modules"],
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production", //pm2 [start|restart|stop|delete] ecosystem.config.js
+      },
+    },
+  ],
+};
