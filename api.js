a94b9df7a7a10834ff3b457cbf9d792ae766ccc9
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2022-11-19 14:52:26 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const axios = require("axios");
 const axiosRetry = require("axios-retry");
 const FormData = require("form-data");
