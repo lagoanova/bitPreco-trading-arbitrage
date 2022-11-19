3e8833b691503c321cd47d204adad64d521813d9
ghostnetrn <54438080+ghostnetrn@users.noreply.github.com>
2022-11-19 14:47:14 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: "bitpreco",
       script: "index.js",
       watch: ["index.js", ".env"],
-      node_args: ["--require dotenv/config"],
       // Delay between restart
       watch_delay: 1000,
       ignore_watch: ["node_modules"],
